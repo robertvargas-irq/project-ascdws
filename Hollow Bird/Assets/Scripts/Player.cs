03d5e237f4f390afca41a17e1ca7eb3d33d93a20
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-03-25 02:30:32 -0600

    Integrated centralized Movement behavior

--- Hollow Bird/Assets/Scripts/Player.cs
+++ Hollow Bird/Assets/Scripts/Player.cs
@@ -2,28 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Movement
 {
-    private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
     private RaycastHit2D hit;
-    public float moveMultiplier = 1;
     public HealthBar HealthBar;
 
     // health for player
     public int maxHealth = 20;
     public int currentHealth;
-    public Inventory inventory = new Inventory();
+    public InventoryManager inventory;
 
     // Start is called before the first frame update
-    private void Start()
+    protected override void Start()
     {
-        boxCollider = GetComponent<BoxCollider2D>();
+        // call parent start
+        base.Start();
 
         // set health for player upon start
         currentHealth = maxHealth;
         HealthBar.SetMaxHealth(maxHealth);
-
 
     }
 
@@ -34,49 +32,15 @@ public class Player : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
 
         // reset moveDelta
-        moveDelta = new Vector3(x, y, 0);
-
-        // swap sprite direction based on direction
-        if (moveDelta.x > 0)
-            transform.localScale = Vector3.one;
-        else if (moveDelta.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-
-        // raycast (Y-AXIS)
-        hit = Physics2D.BoxCast(
-            transform.position,
-            boxCollider.size,
-            0,
-            new Vector2(0, moveDelta.y),
-            Mathf.Abs(moveDelta.y * moveMultiplier * Time.deltaTime),
-            LayerMask.GetMask("Actor", "Blocking")
-        );
-
-        // if no collision from raycast, move sprite
-        if (hit.collider == null)
-            transform.Translate(0, moveDelta.y * moveMultiplier * Time.deltaTime, 0);
-
-        // raycast (X-AXIS)
-        hit = Physics2D.BoxCast(
-            transform.position,
-            boxCollider.size,
-            0,
-            new Vector2(moveDelta.x, 0),
-            Mathf.Abs(moveDelta.x * moveMultiplier * Time.deltaTime),
-            LayerMask.GetMask("Actor", "Blocking")
-        );
-
-        // if no collision from raycast, move sprite
-        if (hit.collider == null)
-        {
-            transform.Translate(moveDelta.x * moveMultiplier * Time.deltaTime, 0, 0);
-        }
+        MoveDirection(new Vector2(x, y));
+        return;
 
     }
 
     // input damage to player or NPC
     void TakeDamage(int damage)
     {
+        // subtract from health
         currentHealth -= damage;
         
         // set damage to healthbar
