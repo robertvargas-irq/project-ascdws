7bd4428142c2a984e4f8d329695c40770fda9b83
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-03-25 02:30:10 -0600

    Added Border to default masks

--- Hollow Bird/Assets/Scripts/Movement.cs
+++ Hollow Bird/Assets/Scripts/Movement.cs
@@ -9,12 +9,13 @@ public class Movement : MonoBehaviour
     private float waterResistance;
     public float moveMultiplier = 0.5F;
     protected string[] blockingMasks;
+    protected bool collisions = true;
 
     // default movement blockers
     protected virtual void Start()
     {
         this.boxCollider = GetComponent<BoxCollider2D>();
-        this.blockingMasks = new string[]{"Actor", "Blocking"};
+        this.blockingMasks = new string[]{"Actor", "Border", "Blocking"};
     }
     public void MoveDirection(Vector2 moveDelta, bool normalized = true)
     {
@@ -29,31 +30,33 @@ public class Movement : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
 
         // raycast (Y-AXIS)
-        hit = Physics2D.BoxCast(
-            transform.position,
-            boxCollider.size,
-            0,
-            new Vector2(0, moveDelta.y),
-            Mathf.Abs(moveDelta.y * moveMultiplier * Time.deltaTime),
-            LayerMask.GetMask(blockingMasks)
-        );
+        if (collisions)
+            hit = Physics2D.BoxCast(
+                transform.position,
+                boxCollider.size,
+                0,
+                new Vector2(0, moveDelta.y),
+                Mathf.Abs(moveDelta.y * moveMultiplier * Time.deltaTime),
+                LayerMask.GetMask(blockingMasks)
+            );
 
         // if no collision from raycast, move sprite
-        if (hit.collider == null)
+        if (!collisions || hit.collider == null)
             transform.Translate(0, moveDelta.y * moveMultiplier * Time.deltaTime, 0);
 
         // raycast (X-AXIS)
-        hit = Physics2D.BoxCast(
-            transform.position,
-            boxCollider.size,
-            0,
-            new Vector2(moveDelta.x, 0),
-            Mathf.Abs(moveDelta.x * moveMultiplier * Time.deltaTime),
-            LayerMask.GetMask(blockingMasks)
-        );
+        if (collisions)
+            hit = Physics2D.BoxCast(
+                transform.position,
+                boxCollider.size,
+                0,
+                new Vector2(moveDelta.x, 0),
+                Mathf.Abs(moveDelta.x * moveMultiplier * Time.deltaTime),
+                LayerMask.GetMask(blockingMasks)
+            );
 
         // if no collision from raycast, move sprite
-        if (hit.collider == null)
+        if (!collisions || hit.collider == null)
         {
             transform.Translate(moveDelta.x * moveMultiplier * Time.deltaTime, 0, 0);
         }
