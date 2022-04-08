f05e52a79f7faa23ab14bbc2598264beea323c00
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-04-07 21:42:31 -0600

    Cleaned up documentation

--- Hollow Bird/Assets/Scripts/Collectable.cs
+++ Hollow Bird/Assets/Scripts/Collectable.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class Collectable : Collidable
 {
     protected bool collected; // collectable object been collected from
+    protected ItemDatabase itemDatabase; // database of all in-game items
 
-    /// <summary>
-    /// Check if collider is a Player.
-    /// </summary>
-    /// <param name="collider"></param>
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+
+        // get the item database
+        itemDatabase = GameManager.instance.itemDatabase;
+    }
+
+    // Check if collider is a Player.
     protected override void OnCollide(Collider2D collider)
     {
         if (collider.name == "Player")
             OnCollect();
     }
 
-    /// <summary>
-    /// Set collected to true.
-    /// </summary>
+    // Set collected to true.
     protected virtual void OnCollect()
     {
         collected = true;
