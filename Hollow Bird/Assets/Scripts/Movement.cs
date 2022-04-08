3d8662cc31541a14089bc81e291b7b264c6c94d5
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-04-07 21:45:09 -0600

    Added Water collisions and slow-downs

--- Hollow Bird/Assets/Scripts/Movement.cs
+++ Hollow Bird/Assets/Scripts/Movement.cs
@@ -6,7 +6,8 @@ public class Movement : MonoBehaviour
 {
     protected BoxCollider2D boxCollider; // the box collider of the child class
     private RaycastHit2D hit;
-    private float waterResistance;
+    private RaycastHit2D waterHit;
+    public float waterResistance = 1.0F;
     public float moveMultiplier = 0.5F;
     protected string[] blockingMasks;
     protected bool collisions = true;
@@ -28,6 +29,17 @@ public class Movement : MonoBehaviour
             transform.localScale = Vector3.one;
         else if (moveDelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
+        
+        // raycast in place for water (Y-AXIS)
+        if (waterResistance != 1.0f)
+            waterHit = Physics2D.BoxCast(
+                transform.position,
+                boxCollider.size,
+                0,
+                new Vector2(0, moveDelta.y),
+                Mathf.Abs(moveDelta.y * moveMultiplier * Time.deltaTime),
+                LayerMask.GetMask("Water")
+            );
 
         // raycast (Y-AXIS)
         if (collisions)
@@ -42,7 +54,18 @@ public class Movement : MonoBehaviour
 
         // if no collision from raycast, move sprite
         if (!collisions || hit.collider == null)
-            transform.Translate(0, moveDelta.y * moveMultiplier * Time.deltaTime, 0);
+            transform.Translate(0, moveDelta.y * moveMultiplier * Time.deltaTime * (waterHit.collider != null ? waterResistance : 1.0f), 0);
+
+        // raycast in place for water (X-AXIS)
+        if (waterResistance != 1.0f)
+            waterHit = Physics2D.BoxCast(
+                transform.position,
+                boxCollider.size,
+                0,
+                new Vector2(moveDelta.x, 0),
+                Mathf.Abs(moveDelta.x * moveMultiplier * Time.deltaTime),
+                LayerMask.GetMask("Water")
+            );
 
         // raycast (X-AXIS)
         if (collisions)
@@ -58,7 +81,8 @@ public class Movement : MonoBehaviour
         // if no collision from raycast, move sprite
         if (!collisions || hit.collider == null)
         {
-            transform.Translate(moveDelta.x * moveMultiplier * Time.deltaTime, 0, 0);
+            transform.Translate(
+                moveDelta.x * moveMultiplier * Time.deltaTime * (waterHit.collider != null ? waterResistance : 1.0f), 0, 0);
         }
     }
 }
