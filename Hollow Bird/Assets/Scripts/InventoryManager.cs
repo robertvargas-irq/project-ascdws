1258980a38860da63ba05e57133506af0917b427
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-04-07 21:43:43 -0600

    Implemented Inventory manager Quickly add and remove items from the player's inventory with weight constraints. Synergizes with ItemDatabase and the Item class.

--- Hollow Bird/Assets/Scripts/InventoryManager.cs
+++ Hollow Bird/Assets/Scripts/InventoryManager.cs
@@ -4,39 +4,92 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    private Hashtable inventoryItemNamesToObjects;
-    private int itemCount = 0;
-    private int currentWeight = 0; // pounds
-    private int maxWeight = 50;    // pounds
-    public bool add(ItemManager item) {
+    public List<Item> characterItems = new List<Item>();
+    public ItemDatabase itemDatabase;
+    public int currentCarryWeight = 0;
+    public int maxCarryWeight = 30;
 
-        // if enough space is available, collect
-        if (currentWeight < maxWeight)
+    // ! TESTING START
+    public void Start()
+    {
+        // for (int i = 0; i < 35; i++)
+        //     GiveItem(0);
+        // RemoveItem(0);
+        // GiveItem(0);
+        // Debug.Log("ITEM COUNT FOR " + itemDatabase.GetItem(0).itemName + ": " + ItemCount(0));
+        // RemoveItem(0);
+        // Debug.Log("ITEM COUNT FOR " + itemDatabase.GetItem(0).itemName + ": " + ItemCount(0));
+    }
+
+    // Get item count for a given item via instance reference
+    public int ItemCount(Item item) {
+        return characterItems.FindAll(i => i.itemId == item.itemId).Count;
+    }
+
+    // Get item count for a given item via ID
+    public int ItemCount(int id) {
+        return ItemCount(itemDatabase.GetItem(id));
+    }
+
+    // Get item count for a given item via Name
+    public int ItemCount(string name) {
+        return ItemCount(itemDatabase.GetItem(name));
+    }
+
+    // Add an item by ID to player's inventory | False if not able to add
+    public bool GiveItem(int id) {
+        return GiveItem(itemDatabase.GetItem(id));
+    }
+
+    // Add an item by Name to player's inventory | False if not able to add
+    public bool GiveItem(string name) {
+        return GiveItem(itemDatabase.GetItem(name));
+    }
+
+    // Add an item by Item instance reference | False if not able to add
+    public bool GiveItem(Item item)
+    {
+        if (CanCarry(item))
         {
-            // add to inventory
-            inventoryItemNamesToObjects[item.itemName] = item;
+            characterItems.Add(item);
+            currentCarryWeight += item.itemWeight;
 
-            // modify inventory fields
-            currentWeight += item.weight;
-            itemCount++;
+            Debug.Log("Added item: " + item.itemName
+            + "\nCurrent Weight: " + currentCarryWeight + " / " + maxCarryWeight);
             return true;
         }
+        // if unable to carry, return false
+        Debug.Log("The player is carrying too much already.");
         return false;
     }
 
-    // Remove from inventory
-    // Returns: True if removed | False if not in the inventory
-    public bool remove(ItemManager item) {
-
-        // if enough space is available, collect
-        if (inventoryItemNamesToObjects.Contains(item.itemName))
-        {
-            inventoryItemNamesToObjects.Remove(item.itemName);
-            currentWeight -= item.weight;
-            itemCount--;
-            return true;
-        }
-        return false;
+    // Remove item by Item ID | False if the item was not in the inventory
+    public bool RemoveItem(int id) {
+        return RemoveItem(itemDatabase.GetItem(id));
     }
 
+    // Remove item by Item Name | False if the item was not in the inventory
+    public bool RemoveItem(string name) {
+        return RemoveItem(itemDatabase.GetItem(name));
+    }
+
+    // Remove item by Item instance reference | False if the item was not in the inventory
+    public bool RemoveItem(Item item) {
+        bool removed = characterItems.Remove(item);
+        if (removed)
+        {
+            currentCarryWeight -= item.itemWeight;
+            Debug.Log("Removed item: " + item.itemName
+            + "\nCurrent Weight: " + currentCarryWeight + " / " + maxCarryWeight);
+        }
+        else Debug.Log("Unable to remove item: ITEM NOT FOUND IN INVENTORY");
+
+        return removed;
+    }
+
+    // Check to see if the player can carry new weight
+    public bool CanCarry(Item item)
+    {
+        return (item.itemWeight + currentCarryWeight) <= maxCarryWeight;
+    }
 }
