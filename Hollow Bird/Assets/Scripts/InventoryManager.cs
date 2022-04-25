be191638d6f58270d2fcdfbec6a05c79e0a14299
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2022-04-24 19:54:59 -0600

    Commented out debug start

--- Hollow Bird/Assets/Scripts/InventoryManager.cs
+++ Hollow Bird/Assets/Scripts/InventoryManager.cs
@@ -12,7 +12,6 @@ public class InventoryManager : MonoBehaviour
     // ! TESTING START
     public void Start()
     {
-        Debug.Log("Current Weight: " + currentCarryWeight + " / " + maxCarryWeight);
         // for (int i = 0; i < 35; i++)
         //     GiveItem(0);
         // RemoveItem(0);
